7d027fe0adaccd93dd13fb7bfe4b52f4820ac49c
IMarcuSI <166658868+IMarcuSI@users.noreply.github.com>
2024-11-06 20:17:17 -0300

    update

--- AppDouc/src/app/pages/registro/registro.page.ts
+++ AppDouc/src/app/pages/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private storage: Storage) { }
 
   ngOnInit() {
   }
 
+
+  formlogin = {
+    rut:"",
+    correo:"",
+    password:""
+  }
+
+
+  Registrarse(){
+    console.log("rut"+ this.formlogin.rut)
+    console.log("correo"+ this.formlogin.rut)
+    console.log("password"+ this.formlogin.password)
+    
+    let datosEnviar: NavigationExtras ={
+      queryParams : {rutUsuario: this.formlogin.rut}
+    }
+
+    this.router.navigate(["/home"] ,datosEnviar)
+
+    this.storage.get(this.formlogin.rut)
+    this.storage.get(this.formlogin.correo)
+    this.storage.get(this.formlogin.password)
+
+  }
 }
