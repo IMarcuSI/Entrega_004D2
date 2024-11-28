90fbc90a4ee131277ece2756cc1a79897a62a4e9
ma.gutierrezk <ma.gutierrezk@duocuc.cl>
2024-11-28 20:35:59 -0300

    jsonserver2.1

--- AppDouc/src/app/guards/auth.guard.spec.ts
+++ AppDouc/src/app/guards/auth.guard.spec.ts
@@ -1,14 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { authGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
-describe('authGuard', () => {
+describe('AuthGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+      TestBed.runInInjectionContext(() => new AuthGuard(
+          TestBed.inject(Router),
+          TestBed.inject(Storage)
+      ).canActivate(...guardParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AuthGuard, Storage]
+    });
   });
 
   it('should be created', () => {
